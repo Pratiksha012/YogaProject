ac354cb53c7af4fed2b691d288896ae048a9dd8b
Pratiksha <88657827+Pratiksha012@users.noreply.github.com>
2024-10-21 09:54:00 +0530

    Update form.js

--- form.js
+++ form.js
@@ -24,7 +24,7 @@ document.getElementById("yogaForm").addEventListener("submit", function(event) {
     }
 
     // Send the data to the server
-    fetch("https://script.google.com/macros/s/AKfycbxyWduznaxeEJ_j7NRTPingVYlrR89b7PGcPtWLlZokcH-a3EdmBc_HZFwoTmnmWyp5Kg/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbw1Ws4pFF5IFgYHzeeyubmMfJUM31R1NPJ6lb0XgLAXUiW24PQTZXIBq9lotrhFx3qS6g/exec", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
